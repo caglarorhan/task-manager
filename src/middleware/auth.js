babcf013e77cf3cddc09fee04a2d029fc83dedf8
caglarorhan <caglaror@gmail.com>
2020-01-27 00:31:25 -0600

    JWT debug

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,10 +4,7 @@ const User = require('../models/user');
 const auth = async (req, res, next)=>{
     try{
         const token = req.header('Authorization').replace('Bearer ','');
-        //res.send(process.env.JWT_SECRETKEY);
         const decoded = jwt.verify(token, process.env.JWT_SECRETKEY);
-        //res.send(decoded);
-        //console.log(decoded);
         const user = await User.findOne({_id: decoded._id, 'tokens.token':token});
         if(!user){
             throw new Error('Can not login!');
